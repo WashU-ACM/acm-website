a7c1f30eca67b63feb250580025ae9a4ab03d1e4
Peter Olson <p.olson@wustl.edu>
2017-02-03 19:32:15 -0600

    Setup gulpfile.js
    Four new gulps: scss, watch, serve, and bsync

--- README.md
+++ README.md
@@ -5,5 +5,21 @@ Powered by [Lumen](https://lumen.laravel.com).
 ## First things
 
 To get started with a developement server running on your localhost, use:
-	php -S localhost:8000 -t public
+    php -S localhost:8000 -t public
 from the root of your project directory.
+
+## Tools
+
+Make sure that you have Composer, NPM, and, of course, PHP installed. Because I (Peter) like gulp, that's what I'm using to automate some stuff.
+
+
+## Gulp
+
+Gulp is pretty useful for developement work. Right now, I just have a few layers of Gulp rules set up. The ones that should be run as `gulp <rule>` are below:
+     scss - compile app.scss and place it in public
+    watch - just watches all .scss files in assets for changes and
+            re-runs scss on change
+    serve - start a php server for the project on localhost:8080
+    bsync - start a php server, and then start a Browser Sync proxy
+            that listens on the same port, so that changes to blades
+            and .scss files are carried through. JS not yet watched.
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+/*
+ *      Gulp Instructions
+ * =============================
+ *     Author - Peter Olson
+ *       Date - 2017-01-27
+ *   Filename - gulpfile.js
+ * =============================
+ */
+
+
+// Requirements
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	php  = require('gulp-connect-php')
+	browserSync = require('browser-sync');
+
+// Configuration variables
+var __bsPort = 80,
+	__bsOpen = "ui", // Change "ui" to false if you don't want the UI to 
+					// pop up every time that browserSync starts
+	__srvPort = 8080;
+
+/*
+ *    Compile scss
+ */
+gulp.task('scss', function(){
+	return gulp.src('resources/assets/app.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('public/css'));
+});
+
+/*
+ *    Start a local PHP server
+ */
+gulp.task('serve', function(){
+	php.server({base: 'public', port: __srvPort});
+});
+
+/*
+ *    Start a PHP server and proxy that with BrowserSync
+ */
+gulp.task('bsync', ['serve', '__bsync-watch'], function(){
+	browserSync({
+		proxy: 'localhost:'concat(__srvPort),
+		port: 80,
+		open: "ui",
+		notify: false,
+		browser: "chrome",
+		files: [
+			"public/css/*.css"
+		]
+	})
+});
+
+/*
+ *    Special watch for browsersync, because, while it can watch css/js for us
+ *       because of the way that lumen seperates blades from the cache, changes
+ *       to blades won't propogate through in a refresh without gulp's help.
+ */
+gulp.task('__bsync-watch', ['watch'], function(){
+	gulp.watch('resources/views/*.blade.php', ['__bs-intern']);
+});
+
+// sorry
+gulp.task('__bs-intern', function(){
+	browserSync.reload();
+});
+
+// Gulp watch function
+gulp.task('watch', function(){
+	gulp.watch('resources/assets/**/*.scss', ['scss']);
+});
