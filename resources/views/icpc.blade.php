beff8a984c464f9537b0394f27d1a2ebe7f2f5c5
mrsata <liuhanrick@gmail.com>
2017-02-13 19:59:01 -0600

    icpc placeholder

---
+++ resources/views/icpc.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title', 'WashU ACM ICPC')
+
+@section('content')
+<div class="container">
+	<h1>International Collegiate Programming Contest</h1>
+	<h2>Coming soon.</h2>
+	<hr>
+	<footer>
+    <p>2017 &copy; WashU ACM </p>
+  </footer>
+</div>
+
+@endsection
