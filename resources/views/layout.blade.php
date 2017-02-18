27825a722f97c54aafa2677014ef1340c58718b5
mrsata <liuhanrick@gmail.com>
2017-02-18 04:11:49 -0600

    Add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
 	<title>@yield('title')</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/app.css">
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
