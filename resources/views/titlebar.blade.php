4bd96754a05e8a63c2e1df1de3eaed317a5d00c5
Han Liu <liuhanrick@gmail.com>
2017-02-17 18:09:13 -0600

    Fix typo

--- resources/views/titlebar.blade.php
+++ resources/views/titlebar.blade.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleContainer" aria-controls="navbarsExampleContainer" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a href="/"><img src="/images/acm_logo_tablet.svg" alt="ACM"></>
+    <a href="/"><img src="/images/acm_logo_tablet.svg" alt="ACM"></a>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
@@ -16,7 +16,7 @@
         <a class="nav-link" href="officers">Officers</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="mailto:acm.wustl.edu">Contact</a>
+        <a class="nav-link" href="mailto:acm@wustl.edu">Contact</a>
       </li>
     </ul>
   </div>
