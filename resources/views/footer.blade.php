bf4129517b3acf59b14147811e6e66154f532dea
mrsata <liuhanrick@gmail.com>
2017-02-20 21:13:56 -0600

    Update footbar: add icons

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,15 @@
-<hr>
 <footer>
-	<p>2017 &copy; WashU ACM </p>
+	<hr>
+	<div class="row">
+    <div class="col-md-4">
+			<a href="https://wustl.edu/"><img src="/images/washu_black.svg" height=30 width=30/></a>
+		</div>
+		<div class="col-md-4">
+			<p class="text-center">2017 &copy; WashU ACM </p>
+		</div>
+		<div class="col-md-4" style="text-align:right">
+			<a href="https://github.com/WashU-ACM"><img src="/images/github_black.svg"  height=25 width=25/></a>
+			<a href="https://www.facebook.com/groups/274789309268731/"><img src="/images/facebook_black.svg"  height=25 width=25/></a>
+			<a href="https://twitter.com/WashUACM"><img src="/images/twitter_black.svg"  height=25 width=25/></a>
+		</div>
 </footer>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,10 +3,12 @@
 <head>
 	<title>@yield('title')</title>
 	<meta charset="utf-8">
+	<meta name="description" content="The Homepage of WashU ACM">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="css/app.css">
 	<link rel="shortcut icon" type="image/png" href="images/favicon.png">
 	<link rel="apple-touch-icon" type="image/png" href="images/apple-touch-icon.png">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
 @include('titlebar')
