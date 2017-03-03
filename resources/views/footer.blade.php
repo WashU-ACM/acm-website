c6a3a86015aa61376445fd93cbd9a74d7229ac3f
mrsata <liuhanrick@gmail.com>
2017-03-03 02:45:07 -0600

    Update footer: close html tag

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
 	<hr>
 	<div class="row">
-    <div class="col-md-4">
+		<div class="col-md-4">
 			<a href="https://wustl.edu/"><img src="/images/washu_black.svg" height=30 width=30/></a>
 		</div>
 		<div class="col-md-4">
@@ -12,4 +12,5 @@
 			<a href="https://www.facebook.com/groups/274789309268731/"><img src="/images/facebook_black.svg"  height=25 width=25/></a>
 			<a href="https://twitter.com/WashUACM"><img src="/images/twitter_black.svg"  height=25 width=25/></a>
 		</div>
+	</div>
 </footer>
