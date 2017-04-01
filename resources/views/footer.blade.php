06cc37177d4eb26f1860d9905198f9ef0a06760e
mrsata <liuhanrick@gmail.com>
2017-04-01 17:42:55 -0500

    Update events

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,15 +2,15 @@
 	<hr>
 	<div class="row">
 		<div class="col-md-4">
-			<a href="https://wustl.edu/"><img src="/images/washu_black.svg" height=30 width=30/></a>
+			<a href="https://wustl.edu/"><img src="/images/washu_black.svg" alt="WashU" height=30 width=30></a>
 		</div>
 		<div class="col-md-4">
 			<p class="text-center">2017 &copy; WashU ACM </p>
 		</div>
 		<div class="col-md-4" style="text-align:right">
-			<a href="https://github.com/WashU-ACM"><img src="/images/github_black.svg"  height=25 width=25/></a>
-			<a href="https://www.facebook.com/groups/274789309268731/"><img src="/images/facebook_black.svg"  height=25 width=25/></a>
-			<a href="https://twitter.com/WashUACM"><img src="/images/twitter_black.svg"  height=25 width=25/></a>
+			<a href="https://github.com/WashU-ACM"><img src="/images/github_black.svg" alt="github" height=25 width=25></a>
+			<a href="https://www.facebook.com/groups/274789309268731/"><img src="/images/facebook_black.svg" alt="facebook" height=25 width=25></a>
+			<a href="https://twitter.com/WashUACM"><img src="/images/twitter_black.svg" alt="twitter" height=25 width=25></a>
 		</div>
 	</div>
 </footer>
