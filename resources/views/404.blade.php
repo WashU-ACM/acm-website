1bae3d42408983f739d0760f2d9b2afcb2691330
mrsata <liuhanrick@gmail.com>
2017-04-20 22:50:01 -0500

    Add 404 page and exception handler

---
+++ resources/views/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title', 'WashU ACM 404')
+
+@section('content')
+<div class="container">
+	<h1>404 Not Found</h1>
+	<p class="lead">Oops. The page you are looking for cannot be found. </p><br>
+	@include('footer')
+</div>
+
+@endsection
