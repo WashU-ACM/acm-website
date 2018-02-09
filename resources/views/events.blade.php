21c2bd68cbb70bce779b99d2928b3ebada9038aa
Han Liu <liuhanrick@gmail.com>
2018-02-08 19:08:05 -0600

    Fix calendar width

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -106,7 +106,7 @@
 
 	<!-- Section: Calendar -->
 	<h2>Calendar</h2>
-	<iframe src="https://calendar.google.com/calendar/embed?src=1n3706c7gs5pdm7f05htfp5k58%40group.calendar.google.com&ctz=America%2FChicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	<iframe src="https://calendar.google.com/calendar/embed?src=1n3706c7gs5pdm7f05htfp5k58%40group.calendar.google.com&ctz=America%2FChicago" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
 
 	<!-- Footer -->
 	@include('footer')
